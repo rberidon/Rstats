cce72cec67d07c1bf4447499231cac3ec2ecbb5e
Rayne Beridon (Raptor) <rberidon@gmail.com>
2012-06-11 06:00:09 -0500

    IP Address addition

--- rstats.sh
+++ rstats.sh
@@ -26,6 +26,11 @@ echo "\"load\"=\"`uptime | grep -o "average.*" | awk '{print $2,$3,$4}'`\"" >> $
 echo "\"uptime\"=\"`uptime | grep -o "up.*:[0-9][0-9]"`\"" >> $LOGFILE
 echo "\"uptime_full\"=\"`uptime`\"" >> $LOGFILE
 
+#IP Address
+echo "\"external_ip\"=\"`curl icanhazip.com`\"" >> $LOGFILE
+echo "\"wireless_ip\"=\"`ifconfig | grep -A 4 en1 | grep -E inet\ | awk '{print $2}'`\"" >> $LOGFILE
+echo "\"wired_ip\"=\"`ifconfig | grep -A 4 en0 | grep -E inet\ | awk '{print $2}'`\"" >> $LOGFILE
+
 #end
 echo "##end#" >> $LOGFILE
 
