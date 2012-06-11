f24519f538b181c8752c67b99fb68fc33c251a8e
Rayne Beridon (Raptor) <rberidon@gmail.com>
2012-06-10 19:27:42 -0500

    Changed filename to log to, now uses hostname

--- rstat-log.sh
+++ rstat-log.sh
@@ -1,25 +1,25 @@
 #Start
-echo "##start#" >> ~/.raptor.log
+echo "##start#" >> `hostname -s`.log
 
 #Date
-echo "\"date\"=\"`date`\"" >> ~/.raptor.log
+echo "\"date\"=\"`date`\"" >> `hostname -s`.log
 
 #Batteries
-/usr/sbin/ioreg -l | grep -e "CycleCount" -e "Capacity" | grep -v "Legacy" | awk '{$1=$2=""; print $0}' | sed 's/^[ \t]*//' >> ~/.raptor.log
+/usr/sbin/ioreg -l | grep -e "CycleCount" -e "Capacity" | grep -v "Legacy" | awk '{$1=$2=""; print $0}' | sed 's/^[ \t]*//' >> `hostname -s`.log
 
 
 ##Averages-Deprecated
-#echo "\"load1\"=\"`uptime | awk '{print $10}'`\"" >> ~/.raptor.log
-#echo "\"load5\"=\"`uptime | awk '{print $11}'`\"" >> ~/.raptor.log
-#echo "\"load15\"=\"`uptime | awk '{print $12}'`\"" >> ~/.raptor.log
-echo "\"load\"=\"`uptime | grep -o "average.*" | awk '{print $2,$3,$4}'`\"" >> ~/.raptor.log
+#echo "\"load1\"=\"`uptime | awk '{print $10}'`\"" >> `hostname -s`.log
+#echo "\"load5\"=\"`uptime | awk '{print $11}'`\"" >> `hostname -s`.log
+#echo "\"load15\"=\"`uptime | awk '{print $12}'`\"" >> `hostname -s`.log
+echo "\"load\"=\"`uptime | grep -o "average.*" | awk '{print $2,$3,$4}'`\"" >> `hostname -s`.log
 
 #Uptime
-echo "\"uptime\"=\"`uptime | grep -o "up.*:[0-9][0-9]"`\"" >> ~/.raptor.log
-echo "\"uptime_full\"=\"`uptime`\"" >> ~/.raptor.log
+echo "\"uptime\"=\"`uptime | grep -o "up.*:[0-9][0-9]"`\"" >> `hostname -s`.log
+echo "\"uptime_full\"=\"`uptime`\"" >> `hostname -s`.log
 
 #end
-echo "##end#" >> ~/.raptor.log
+echo "##end#" >> `hostname -s`.log
 
 #newline
-echo >> ~/.raptor.log
+echo >> `hostname -s`.log
