fd13915059b930cee952b980d26c796911d400b4
Rayne Beridon <rberidon@gmail.com>
2013-07-03 18:31:14 -0500

    Update from a year's worth of miscellaneous changes without corresponding Git history. Rstats has been living quite happily on my laptop, collecting away.

--- rstats.sh
+++ rstats.sh
@@ -25,18 +25,19 @@ echo "\"time\"=\"`date | awk '{print $4}'`\"" >> $LOGFILE
 #echo "\"load1\"=\"`uptime | awk '{print $10}'`\"" >> $LOGFILE
 #echo "\"load5\"=\"`uptime | awk '{print $11}'`\"" >> $LOGFILE
 #echo "\"load15\"=\"`uptime | awk '{print $12}'`\"" >> $LOGFILE
-echo "\"load\"=\"`uptime | grep -o "average.*" | awk '{print $2,$3,$4}'`\"" >> $LOGFILE
+echo "\"load\"=\"`uptime 2>>/dev/null | grep -o "average.*" | awk '{print $2,$3,$4}'`\"" >> $LOGFILE
 
 
 #Uptime
-echo "\"uptime\"=\"`uptime | grep -o "up.*:[0-9][0-9]"`\"" >> $LOGFILE
-echo "\"uptime_full\"=\"`uptime`\"" >> $LOGFILE
+echo "\"uptime\"=\"`uptime 2>>/dev/null | grep -o "up.*:[0-9][0-9]"`\"" >> $LOGFILE
+echo "\"uptime_full\"=\"`uptime 2>>/dev/null `\"" >> $LOGFILE
 
 
-#IP Address
+#Networking
 echo "\"external_ip\"=\"`curl -sf icanhazip.com`\"" >> $LOGFILE
-echo "\"wireless_ip\"=\"`/sbin/ifconfig 2>>/dev/null| grep -A 4 en1 | grep inet\ | awk '{print $2}'`\"" >> $LOGFILE
-echo "\"wired_ip\"=\"`/sbin/ifconfig 2>>/dev/null| grep -A 4 en0 | grep inet\ | awk '{print $2}'`\"" >> $LOGFILE
+echo "\"wireless_ip\"=\"`/sbin/ifconfig 2>>/dev/null| grep -A 4 en0 | grep inet\ | awk '{print $2}'`\"" >> $LOGFILE
+echo "\"wired_ip\"=\"`/sbin/ifconfig 2>>/dev/null| grep -A 4 en1 | grep inet\ | awk '{print $2}'`\"" >> $LOGFILE
+echo "\"ssid\"=\"`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I 2>>/dev/null | grep \ SSID | sed "s/[\ ]*SSID:[\ ]*//"`\"" >> $LOGFILE
 
 
 #Highest-load program
@@ -51,6 +52,9 @@ echo "\"disk1t\"=\"`df -h | grep \/$ | awk '{print $2}'`\"" >> $LOGFILE
 echo "\"disk2f\"=\"`df -h | grep \/data$ | awk '{print $4}'`\"" >> $LOGFILE
 echo "\"disk2t\"=\"`df -h | grep \/data$ | awk '{print $2}'`\"" >> $LOGFILE
 
+
+#Displays
+echo "\"screen_resolution_primary\"=\"`/usr/sbin/system_profiler SPDisplaysDataType 2>>/dev/null | grep Resolution | sed 's/^[[:space:]]*Resolution:\ //g' | head -n 1`\"" >> $LOGFILE
 
 #end
 echo "##end#" >> $LOGFILE
